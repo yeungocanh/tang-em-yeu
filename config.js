51b347df93d6cf4f071ad311030da2ce118ccf71
Minh Hieu <55304715+tomken13@users.noreply.github.com>
2021-06-24 00:00:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Em yêu à!',
+    introDesc: ` Anh không biết nói sao cho em hiểu nên anh làm cho em cái vày nè`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Anh biết em yêu anh nhưng anh thì cũng không biết làm gì cả anh chỉ có cái này tặng em',
+    desc: 'Em yêu anh lắm phải không ',
+    btnYes: 'Vẫn cứ là yêu anh <3',
+    btnNo: 'Em yêu anh nhiều <3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Anh yêu em lắm đó <3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.messenger.com/t/100008065122193'
+
+```
+
