61781fa407c52852fc5ac7e25f90288aee18c966
yeungocanh <86422150+yeungocanh@users.noreply.github.com>
2021-06-25 01:17:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function firstQuestion(){
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        videoUrl: 'sound/UUXA9114 (1).mp4',
+        imgUrl: 'img/lookMe.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("img/iput-bg.jpg")',
