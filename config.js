30273680916bd918f1587416b54a7cbd056d38ef
yeungocanh <86422150+yeungocanh@users.noreply.github.com>
2021-06-25 00:00:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ const CONFIG = {
     introDesc: `Hôm nay là một ngày đặc biệt của em mặc dù anh không được ở cạnh em nhưng mà anh rất yêu em❤
     Anh đang rất mong muốn để được gặp em nhưng anh lại không thể Anh xin lỗi em nhưng em ơi anh yêu em nhiều hơn những gì em nghĩ đó`,
     btnIntro: 'Yêu anh ❤',
-    title: 'Chúc mừng sinh nhật người yêu nhất và ngoan hiền dịu ngọt nhất của anh. Cuộc sống buồn tẻ biết bao nếu vắng em.
-    Cảm ơn em! Người mang cho anh niềm vui trong cuộc sống và hứng khởi trong mọi việc. Sinh nhật em Anh chúc em luôn xinh đẹp và yêu đời. Anh sẽ luôn bên em! 😙❤ ',
+    title: 'Chúc mừng sinh nhật người anh yêu nhất và ngoan hiền dịu ngọt nhất của anh. Cuộc sống buồn tẻ biết bao nếu vắng em. Anh cảm ơn mẹ em vì đã xinh ra một cô công chúa xinh đẹp và giờ là người yêu của anh. Anh sẽ tặng em món quà quý giá nhất của anh đó là... cuộc đời của anh 😙❤ ',
     desc: 'Anh yêu em 😘',
     btnYes: 'Em yêu anh ❤',
     btnNo: 'Không yêu anh đâu',
