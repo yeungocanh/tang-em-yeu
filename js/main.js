f5a17bbaca912f5dc3327a09e07bc73939a9073b
yeungocanh <86422150+yeungocanh@users.noreply.github.com>
2021-06-25 03:32:22 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ $('#yes').click(function() {
         background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
-              url("img/giphy2.gif")
+              url("img/em.gif")
               left top
               no-repeat
             `,
